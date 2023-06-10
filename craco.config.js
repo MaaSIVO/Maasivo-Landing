0f20ad4eeda29b721660cebd04d17cc59d50db29
AnaidOrtega <itzel.ortega9819@hotmail.com>
2023-06-09 19:31:02 -0600

    CONFIG: Add Path Alias With Craco

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+}
